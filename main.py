85620b1e919e2e0b2095e0307bade49d42b3617a
Kevin <jesikamaraj@gmail.com>
2024-06-04 14:33:08 +0530

    chore: Update database name and add username replacer functionality in main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import asyncio
-from pyrogram import Client, filters, errors
+from pyrogram import Client, filters, errors, idle
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from motor.motor_asyncio import AsyncIOMotorClient
 import pyromod
 import os
 from dotenv import load_dotenv
 
+import re
+
 load_dotenv()
 
 # Configuration
 api_id = int(os.getenv("API_ID", "11450835"))
 api_hash = os.getenv("API_HASH", "0fadb61feae6ccf016932823bbf1565c")
-bot_token = os.getenv("BOT_TOKEN", "7352445727:AAE4uMuzaPG1ZzX5fsvufsF_GJKoWc3GEAY")
-mongo_uri = os.getenv("DATABASE_URL", "mongodb+srv://ALEX1206:ALEX1206@cluster0.ipmgpii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+bot_token = os.getenv("BOT_TOKEN", "5404466045:AAF3_uF-D_AKLnKwctQborke_tMJFnFMXvM")
+mongo_uri = os.getenv(
+    "DATABASE_URL",
+    "mongodb+srv://ALEX1206:ALEX1206@cluster0.ipmgpii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+)
 db_name = os.getenv("DATABASE_NAME", "word_replacer")
 
 
@@ -40,6 +45,20 @@ class Database:
             for doc in await cursor.to_list(length=None)
         }
 
+    async def add_replace_username(self, username):
+        await self.collection.update_one(
+            {"category": "username", "username": username},
+            {"$set": {"username": username}},
+            upsert=True,
+        )
+
+    async def remove_replace_username(self, username):
+        await self.collection.delete_one({"category": "username", "username": username})
+
+    async def get_replace_username(self):
+        cursor = await self.collection.find_one({"category": "username"})
+        return cursor.get("username") if cursor else None
+
 
 # Initialize Pyrogram Client and Database
 app = Client("word_replacer_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
@@ -47,19 +66,23 @@ db = Database(mongo_uri)
 
 
 # Start command
-@app.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message: Message):
     replace_words = await db.get_words("replace")
     remove_words = await db.get_words("remove")
+    username = await db.get_replace_username()
 
-    text = "✨ Welcome to the Word Replacer Bot! ✨\n\n"
-    text += "🔄 **Replace Words**:\n"
+    text = "✨ Welcome to the Word Replacer Bot! ✨\n\n" + "🔄 **Replace Words**:\n"
     for word, replacement in replace_words.items():
         text += f"`{word}` ➡️ `{replacement}`\n"
 
     text += "\n❌ **Remove Words**:\n"
     for word in remove_words:
         text += f"`{word}`\n"
+
+    text += "\n👤 **Username Replacer**:\n"
+    text += f"`{username}`\n"
+
     text += "\nUse /addword to add or update words and /removeword to remove words."
     await message.reply_text(text)
 
@@ -71,20 +94,50 @@ async def add_word(client, message: Message):
         [
             [InlineKeyboardButton("🔄 Replace Words", callback_data="add_replace")],
             [InlineKeyboardButton("❌ Remove Words", callback_data="add_remove")],
+            [
+                InlineKeyboardButton(
+                    "👤 Username Replacer", callback_data="add_username"
+                )
+            ],
             [InlineKeyboardButton("🔙 Back", callback_data="back")],
         ]
     )
     await message.reply_text("Choose the category:", reply_markup=category_buttons)
 
 
+@app.on_callback_query(filters.regex(r"add_username"))
+async def process_add_username(client, callback_query):
+    text = "Send the username you want to replace."
+    try:
+        username_msg = await client.ask(callback_query.message.chat.id, text)
+
+        if username_msg.text == "/cancel":
+            await callback_query.message.reply_text(
+                "❌ Cancelled.", reply_markup=back_button()
+            )
+            return
+
+        await db.add_replace_username(username_msg.text)
+        await callback_query.message.reply_text(
+            "✅ Username added successfully.",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🔙 Back", callback_data="back")]]
+            ),
+        )
+    except Exception as e:
+        await callback_query.message.reply_text(f"❌ An error occurred: {e}")
+
+
 @app.on_callback_query(filters.regex(r"add_(replace|remove)"))
 async def process_add_word(client, callback_query):
-    category = "replace" if "replace" in callback_query.data else "remove"
+    category = callback_query.data.split("_")[1]
 
     category_words = await db.get_words(category)
 
-    text = "Send the word you want to add.\n\n"
-    text += "📜 **Words in the selected category**:\n"
+    text = (
+        "Send the word you want to add.\n\n"
+        + "📜 **Words in the selected category**:\n"
+    )
     for word in category_words:
         text += f"\n`{word}`"
     text += "\n\nType /cancel to cancel."
@@ -121,10 +174,43 @@ async def remove_word(client, message: Message):
         [
             [InlineKeyboardButton("🔄 Replace Words", callback_data="remove_replace")],
             [InlineKeyboardButton("❌ Remove Words", callback_data="remove_remove")],
+            [
+                InlineKeyboardButton(
+                    "👤 Username Replacer", callback_data="remove_username"
+                )
+            ],
             [InlineKeyboardButton("🔙 Back", callback_data="back")],
         ]
     )
     await message.reply_text("Choose the category:", reply_markup=category_buttons)
+
+
+@app.on_callback_query(filters.regex(r"remove_username"))
+async def process_remove_username(client, callback_query):
+    try:
+        username = await db.get_replace_username()
+        text = f"👤 **Username Replacer**:\n\n`{username}`"
+        await callback_query.message.reply_text(text)
+
+        confirm_msg = await client.ask(
+            callback_query.message.chat.id,
+            "Do you want to remove the username replacer? (yes/no)",
+        )
+
+        if confirm_msg.text == "yes":
+            await db.remove_replace_username(username)
+            await callback_query.message.reply_text(
+                "✅ Username removed successfully.",
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton("🔙 Back", callback_data="back")]]
+                ),
+            )
+        else:
+            await callback_query.message.reply_text(
+                "❌ Operation cancelled.", reply_markup=back_button()
+            )
+    except Exception as e:
+        await callback_query.message.reply_text(f"❌ An error occurred: {e}")
 
 
 @app.on_callback_query(filters.regex(r"remove_(replace|remove)"))
@@ -203,6 +289,10 @@ async def batch_process(client: Client, message: Message):
                 for word in remove_words:
                     text = text.replace(word, "")
 
+                input_replace_username = await db.get_replace_username()
+                if input_replace_username:
+                    text = replace_username(input_replace_username, text)
+
                 if text == og_text:
                     continue
 
@@ -244,6 +334,10 @@ async def process_message(client, message: Message):
     for word in remove_words:
         text = text.replace(word, "")
 
+    input_replace_username = await db.get_replace_username()
+    if input_replace_username:
+        text = replace_username(input_replace_username, text)
+
     if message.text:
         await message.reply_text(text)
     else:
@@ -261,6 +355,16 @@ def back_button():
         [[InlineKeyboardButton("🔙 Back", callback_data="back")]]
     )
 
+
+def replace_username(username, text):
+    pattern = re.compile(r"@(\w+)")
+    username = f"@{username}"
+    return pattern.sub(username, text)
+
+
 print("Bot started!")
-# Run the bot
-app.run()
+app.start()
+print(f"Username: {app.get_me().username}")
+idle()
+app.stop
+
